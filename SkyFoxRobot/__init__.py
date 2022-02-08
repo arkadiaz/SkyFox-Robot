f65223a4c98c81432682f80061edb6cd69264537
Arka <96603361+arkadiaz@users.noreply.github.com>
2022-02-08 13:33:54 +0700

    Update __init__.py

--- SkyFoxRobot/__init__.py
+++ SkyFoxRobot/__init__.py
@@ -218,7 +218,7 @@ else:
         sw = None
         LOGGER.warning("Can't connect to SpamWatch!")
 
-from SkyFox.modules.sql import SESSION
+from SkyFoxRobot.modules.sql import SESSION
 
 defaults = tg.Defaults(run_async=True)
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
